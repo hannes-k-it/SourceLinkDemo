99a5d4f6bbf67af9e21a675de3bc596dcea4a55f
Hannes Karask <hannes.karask@gmail.com>
2018-05-25 11:46:58 +1000

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SourceLinkDemoDist
+{
+    public class Class1
+    {
+        /// <summary>
+        /// Some summary
+        /// </summary>
+        public void Foo()
+        {
+            Console.WriteLine("bar");
+        }
+    }
+}
